c5778bda463e11999a2b58f8e07e592b29599168
maina-dennis <mainajjm0@gmail.com>
2022-05-29 12:22:46 +0300

    update userfactory to new syntax

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,11 +3,19 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
     /**
      * Define the model's default state.
      *
